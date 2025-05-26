6ddacdbce8cfcb04037887e0272df8a1fdc9aaad
Titilope Olatunji <titilopeolatunji2013@gmail.com>
2025-05-26 16:25:11 +0100

    Update vpc file

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 #vpc setup
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "titilope_vpc" {
   cidr_block = "10.2.0.0/16"
 
   tags = {
@@ -10,7 +10,7 @@ resource "aws_vpc" "my_vpc" {
 
 #created public and private subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id     = aws_vpc.titilope_vpc.id
   cidr_block = "10.2.0.0/24"
 
   tags = {
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
+  vpc_id     = aws_vpc.titilope_vpc.id
   cidr_block = "10.2.2.0/24"
 
   tags = {
@@ -29,7 +29,7 @@ resource "aws_subnet" "private_subnet" {
 
 #Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.titilope_vpc.id
 
   tags = {
     Name = "Titilope"
@@ -38,7 +38,7 @@ resource "aws_internet_gateway" "igw" {
 
 #Route table
 resource "aws_route_table" "titilope_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.titilope_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -64,31 +64,17 @@ resource "aws_route_table_association" "public_association" {
 resource "aws_security_group" "security_group" {
   name        = "my_security_group"
   description = "Allow inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.titilope_vpc.id
   
   tags = {
     Name = "allow_ssh"
-  }
-
-  ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.security_group.id
-  cidr_ipv4         = aws_vpc.my_vpc.cidr_block
+  cidr_ipv4         = aws_vpc.titilope_vpc.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
